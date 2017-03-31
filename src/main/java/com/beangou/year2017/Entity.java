73f1c370586c973fa091d0271e9725086384f7e6
liutongbin <liutongbin@loonxi.com>
2017-03-31 16:40:42 +0800

    test subList

---
+++ src/main/java/com/beangou/year2017/Entity.java
@@ -0,0 +1,21 @@
+package com.beangou.year2017;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * <pre>
+ * 说明：
+ *
+ * </pre>
+ *
+ * @author <a href="mailto:liutongbin@loonxi.com">小彬</a>
+ * @version 1.0 2017/3/31
+ * @since 1.0
+ */
+public class Entity {
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+}
