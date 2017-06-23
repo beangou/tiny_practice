760bdc4f743750b323304ba7e6d6f08bdf2aa029
liutongbin <liutongbin@loonxi.com>
2017-06-23 18:39:21 +0800

    testArray

--- src/main/java/com/beangou/year2017/day0207/TestThreadPool.java
+++ src/main/java/com/beangou/year2017/day0207/TestThreadPool.java
@@ -2,6 +2,10 @@ package com.beangou.year2017.day0207;
 
 import org.junit.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 /**
  * Created by liutb on 2017/2/6.
  *
@@ -12,6 +16,11 @@ public class TestThreadPool {
     @Test
     public void testThreadPoolExecutor() {
 //        ThreadPoolExecutor executor = new ThreadPoolExecutor();
+        // 线程池如何管理线程，如何知道线程池里面所有线程当前状态，正在运行？ 阻塞
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+         Future future = executorService.submit(() -> {
+
+         });
 
     }
 
---
+++ src/main/java/com/beangou/year2017/day0623/TestArray.java
@@ -0,0 +1,26 @@
+package com.beangou.year2017.day0623;
+
+import org.junit.Test;
+
+/**
+ * <pre>
+ * 说明：
+ *
+ * </pre>
+ *
+ * @author <a href="mailto:liutongbin@loonxi.com">小彬</a>
+ * @version 1.0 2017/6/23
+ * @since 1.0
+ */
+public class TestArray {
+
+    @Test
+    public void initArr() {
+        Object[] objs = {new Object(), new Object()};
+
+//        compile error.
+//        Object[] objs2;
+//        objs2 = {new Object(), new Object()};
+    }
+
+}
